b56e05e9d186e3225bffd2f7378690513e921a8c
jiju278 <oneale67@gmail.com>
2024-04-13 14:26:17 +0200

    Add login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,54 @@
+import Button from '@/components/ui/Button/Button';
+import logo from '@assets/white_frame.png';
+import style from '@pages/Login/Login.module.css';
+import { FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style['logo-section']}>
+        <img className={style.logo} src={logo} alt="logo" />
+      </div>
+      <div className={style['signin-section']}>
+        <div className={style['signin-form']}>
+          <h2 className={style['form-title']}>Sign in</h2>
+          <form className={style.form} onSubmit={handleSubmit}>
+            <label className={style.label} htmlFor="email">
+              Email
+            </label>
+            <input
+              className={style.input}
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
+
+            <label className={style.label} htmlFor="password">
+              Password
+            </label>
+            <input
+              className={style.input}
+              type="password"
+              id="password"
+              name="password"
+              required
+            />
+
+            <Button color="primary" title="Sign in" />
+          </form>
+          <p>
+            Don't have an account? <Link to="/signup">Sign up</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
